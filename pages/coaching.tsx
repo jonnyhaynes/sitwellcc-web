c9357102e7876e1ba43b96fb58c5772ee8346959
Jonny Haynes <jonny@hma.co.uk>
2022-10-17 14:13:38 +0100

    ✏️ Copy changes

--- pages/coaching.tsx
+++ pages/coaching.tsx
@@ -85,7 +85,7 @@ const Coaching: NextPage = () => {
                 </section>
                 <section className="mb-5 md:mb-0 md:col-span-2 border-t-8 border-blue pt-5 px-5 bg-blue bg-opacity-5">
                     <h2 className="text-3xl font-ropa">Talent Development</h2>
-                    <p className="mb-2">Using the British Cycling talent development pathways we teach your children the fundamentals of cycling before coaching them on their favoured discipline. We do fun activities and games using cones, bean bags, tennis balls, limbo bars, ramps etc to enhance the cycling skills of your child and give them a good foundation for the future.</p>
+                    <p className="mb-2">Using the British Cycling talent development pathways we teach your children the fundamentals of cycling before coaching them on their favoured discipline. We do fun activities and games using cones, bean bags, balance baords, limbo bars, ramps etc to enhance the cycling skills of your child and give them a good foundation for the future.</p>
                     <p className="mb-2">Our more advanced students are using BMX style pump tracks, taking on trail centers and we&apos;re looking this year how to get some road cycling done on quiet lanes.</p>
                     <p className="mb-2"><strong>Pedal sessions</strong> are for children who no longer use stabilisers or a balance bike and are looking to increase their confidence. Pedal sessions are run 10-11:15.</p>
                     <p className="mb-5">Our <strong>Balance sessions</strong> are for children new to cycling, who may have just had their stabilisers taken off or who are still using a balance bike. Balance sessions are run 11:15-12.</p>
@@ -102,7 +102,7 @@ const Coaching: NextPage = () => {
                     </section>
                     <h2 className="text-3xl font-ropa">Herringthorpe</h2>
                     <p className="mb-2">We use various sections of Herringthorpe Playing Fields. Please lookout for the SCC tent or flag.</p>
-                    <p className="mb-5">Free off-street parking is available just off Middle Lane South towards the junction with Dryden Road.</p>
+                    <p className="mb-2">Free off-street parking is available just off Middle Lane South towards the junction with Dryden Road.</p>
                     <p className="mb-5">We announce the week before on social media and our Slack channels where we&apos;ll be on Herringthorpe.</p>
                     <h2 className="text-3xl font-ropa">Requirements</h2>
                     <p className="mb-2">There&apos;s no minimum age requirement for these sessions other than can your child follow instructions. Our youngest member is three years old. These aren&apos;t sessions for teaching your child to ride a bike, but to enhance their skills and grow their confidence. If you&apos;re not sure, bring them along. We can provide advice and guidance no matter what their cycling ability.</p>
@@ -111,6 +111,8 @@ const Coaching: NextPage = () => {
                     <p className="mb-5">We are proud to be a validated Learning Destination. Collect CU credits at our activities with your Passport to Learning. There is more on <a href="https://www.childrensuniversity.co.uk/universities/rotherham-childrens-university/">Rotherham Children&apos;s University</a> here.</p>
                     <h2 className="text-3xl font-ropa">The Coalfields Regeneration Trust</h2>
                     <p className="mb-5">The Coalfields Regeneration Trust has provided funding to train 3 British Cycling Level 1 coaches and progress 3 coaches to Level 2. More can be found on <a href="https://www.coalfields-regen.org.uk/">The Coalfields Regeneration Trust</a> here.</p>
+                    <h2 className="text-3xl font-ropa">Community Leadership Fund</h2>
+                    <p className="mb-5">Rotherham East ward have provided funding to train two new coaches in emergency first aid.</p>
                     <h2 className="text-3xl font-ropa">Safeguarding</h2>
                     <p className="mb-2">All coaches hold relevant qualifications and insurance through British Cycling. They are first aid trained, DBS checked and they&apos;ve done safeguarding training.</p>
                     <p className="mb-5">We also have <Link href="/welfare"><a>Club Welfare Officers</a></Link> and follow British Cycling&apos;s Safeguarding policies.</p>
